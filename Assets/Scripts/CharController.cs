2ffaf49e80515b0792c5c128cf1d790d0e31d7b0
PseudoscienceGames <Quade81@gmail.com>
2017-12-10 10:58:26 -0600

    Character Controller start

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+	public float moveSpeed;
+	private CharacterController c;
+
+	private void Start()
+	{
+		c = GetComponent<CharacterController>();
+	}
+
+	void Update ()
+	{
+		Vector3 mov = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		c.Move(transform.TransformVector(mov) * Time.deltaTime * moveSpeed);
+	}
+}
