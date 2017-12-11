821b4bcbb250c2e2292e98845e4ca098922778b4
PseudoscienceGames <Quade81@gmail.com>
2017-12-11 14:37:17 -0600

    Added block & empty holding test

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CharController : MonoBehaviour
 {
 	public float moveSpeed;
+	public Vector3 mov;
 	private CharacterController c;
+	public bool holding = false;
 
 	private void Start()
 	{
@@ -14,7 +16,28 @@ public class CharController : MonoBehaviour
 
 	void Update ()
 	{
-		Vector3 mov = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		c.Move(transform.TransformVector(mov) * Time.deltaTime * moveSpeed);
+		if (Input.GetMouseButtonDown(0))
+			CheckForMovableBlock();
+		if (Input.GetMouseButtonUp(0))
+			holding = false;
+		if (holding)
+		{
+
+		}
+		else
+		{
+			mov = new Vector3(Input.GetAxis("Horizontal"), mov.y, Input.GetAxis("Vertical"));
+			if (!c.isGrounded)
+				mov += Physics.gravity * Time.deltaTime;
+			else
+				mov.y = 0;
+			c.Move(transform.TransformVector(mov) * Time.deltaTime * moveSpeed);
+		}
+	}
+
+	void CheckForMovableBlock()
+	{
+
+		holding = true;
 	}
 }
