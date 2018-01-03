2dbc09b47739a9bc1fc741dfd3babf3a94584030
PseudoscienceGames <quade.81@gmail.com>
2018-01-02 23:28:20 -0600

    Removed shit and started new meshes

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,43 +5,17 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
 	public GridLoc gridLoc;
-	public Dictionary<GridLoc, Side> sides = new Dictionary<GridLoc, Side>();
 	public bool ss = false;
 	public bool active = false;
 
 	public virtual void Start()
 	{
 		gridLoc = new GridLoc(transform.position);
-		AddSides();
 		GridController.instance.grid.Add(gridLoc, this);
-	}
-	public virtual void AddSides()
-	{
-		sides.Add(GridLoc.up, new Side(SideType.Base, this, GridLoc.up));
-		sides.Add(GridLoc.down, new Side(SideType.Base, this, GridLoc.down));
-		sides.Add(GridLoc.right, new Side(SideType.Base, this, GridLoc.right));
-		sides.Add(GridLoc.left, new Side(SideType.Base, this, GridLoc.left));
-		sides.Add(GridLoc.forward, new Side(SideType.Base, this, GridLoc.forward));
-		sides.Add(GridLoc.back, new Side(SideType.Base, this, GridLoc.back));
-		foreach(GridLoc g in sides.Keys)
-		{
-			Debug.Log(g + " " + FindLocalSideGridLoc(g));
-		}
 	}
 	public virtual void Action(GridLoc g)
 	{
 		active = true;
-	}
-
-	public GridLoc FindWorldSideGridLoc(GridLoc localSide)
-	{
-		GridLoc worldSide = new GridLoc(transform.rotation * localSide.ToVector3());
-		return worldSide;
-	}
-	public GridLoc FindLocalSideGridLoc(GridLoc worldSide)
-	{
-		GridLoc localSide = new GridLoc(Quaternion.Inverse(transform.rotation) * worldSide.ToVector3());
-		return localSide;
 	}
 
 	public virtual void EndLastTick()
