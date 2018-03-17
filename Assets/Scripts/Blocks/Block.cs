cff4f3619bd0ff8540feeb07f30b530fdd24a782
PseudoscienceGames <Quade81@gmail.com>
2018-03-17 12:36:33 -0500

    added colors

--- Assets/Scripts/Blocks/Block.cs
+++ Assets/Scripts/Blocks/Block.cs
@@ -7,7 +7,7 @@ public class Block : MonoBehaviour
 	public Vector3Int loc;
 	public bool grabbed;
 	public bool locked;
-	public int color;//0=red 1=orange 2=yellow 3=green 4=blue 5=purple
+	public int color;//0=any 1=red 2=orange 3=yellow 4=green 5=blue 6=purple
 
 	public virtual void Activate(float time)
 	{
